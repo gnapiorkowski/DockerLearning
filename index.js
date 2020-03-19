9a056012b9eb228416a48e1b8a77687eb292237a
Gustaw <56068621+gnapiorkowski@users.noreply.github.com>
2020-03-19 13:34:40 +0100

    minor fixes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = reqquire('express')
-
+const express = require('express');
+ 
 const app = express();
-
+ 
 app.get("/", (req, resp) => {
-	resp.send("Hello WORDLD _______");
+    resp.send("Hello WORLD -----");
 });
-
+ 
 app.listen(4000, () => {
-	console.log("Ser listening on port 4000")
+    console.log("Server listening on port 4000");
 });
